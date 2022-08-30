1cac2944c25c0b61641895a20dcd1532787b2615
Zehn <z3hndolphin@gmail.com>
2022-08-29 20:36:58 -0400

    P

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ console.log(totalPrice)
 
 
 const people = [
-    { name: 'Kyle', age: 26 },
+    { name: 'Kyle', age: 66 },
     { name: 'John', age: 31 },
     { name: 'Sally', age: 42 },
-    { name: 'Jill', age: 42 },
+    { name: 'Jill', age: 60 },
 ]
 
 const result = people.reduce((groupedPeople, person) => {

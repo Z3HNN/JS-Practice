c424749ca3e7f3e949221d0fbe9484bb6c39c51f
Zehn <z3hndolphin@gmail.com>
2022-08-30 23:24:22 -0400

    p

--- practice.js
+++ practice.js
@@ -6,7 +6,7 @@ function bouncer(arr) {
 
 let myArr = [1, 2, 4, 1]
 let myOutput = myArr.filter(function(item){
-    return item < 6
+    return item < 2
 })
 
 let output = bouncer(['a', 'b', null, 1])

5c398882a013cd8dbc1a602ec6719dc3d83f6c72
Zehn <z3hndolphin@gmail.com>
2022-08-30 23:19:12 -0400

    p

--- practice.js
+++ practice.js
@@ -1,11 +1,12 @@
 function bouncer(arr) {
+    let newArr = []
 for(let i = 0; i < arr.length; i++){
     console.log(arr[i])
+    if(arr[i]) {
+        newArr.push(arr[i])
+    }
 }
-}
-
-if (true) {
-    console.log('hi')
+return newArr
 }
 
 let output = bouncer(['a', 'b', null])

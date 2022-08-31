4c005e963cfa3ee7d0e62b93e7c3632817b485da
Zehn <z3hndolphin@gmail.com>
2022-08-30 23:23:10 -0400

    p

--- practice.js
+++ practice.js
@@ -1,4 +1,7 @@
 function bouncer(arr) {
+    return arr.filter(function(item){
+        return item
+    })
 }
 
 let myArr = [1, 2, 4, 7, 1]
@@ -6,5 +9,5 @@ let myOutput = myArr.filter(function(item){
     return item < 3
 })
 
-let output = bouncer(['a', 'b', null])
+let output = bouncer(['a', 'b', null, 1])
 console.log(output)

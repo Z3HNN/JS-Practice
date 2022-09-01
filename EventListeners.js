a6ba0c9e7cc6032e8f1fcdabacf6fa61642cb7c2
Zehn <z3hndolphin@gmail.com>
2022-08-31 22:41:01 -0400

    p

--- EventListeners.js
+++ EventListeners.js
@@ -1,5 +1,5 @@
 const grandparent = document.querySelector(".grandparent")
-const parent = document.querySelector(".parent")
+const parent = document.querySelector(".Adult")
 const child = document.querySelector(".child")
 
 grandparent.addEventListener("click", e => {

39677a74f8c65e56a7175d3b74ed59f539634fc9
Zehn <z3hndolphin@gmail.com>
2022-09-03 12:24:16 -0400

    p

--- sumallnum.js
+++ sumallnum.js
@@ -1,6 +1,11 @@
 function sumAll(arr) {
  let minArr = Math.min(arr[0], arr[1])
- let maxArr = Math.max(arr[0])
+ let maxArr = Math.max(arr[0], arr[1])
+ let total = 0
+ for (let i = minArr; i <= maxArr; i++) {
+    total += i
+ }
+ return total
 }
 
 let output = sumAll([1, 3])

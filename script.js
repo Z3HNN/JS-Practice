708beeb5831fee6e5ed2665effa0cf39be6407ee
Zehn <z3hndolphin@gmail.com>
2022-09-13 22:19:34 -0400

    p

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ console.log(sum)
 
 const bakeryItems = [
     { product: "brownies", price: 7 },
-    { product: "cookies", price: 6 },
+    { product: "cookies", price: 2 },
     { product: "bread", price: 3 },
     { product: "cake", price: 7},
 ]
